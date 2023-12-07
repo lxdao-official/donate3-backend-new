e8882bd72031bdac8f0f9ecbfd4debab8384e84e
wwei-github <418047394@qq.com>
2023-12-07 15:48:31 +0800

    feat: test

--- src/api/settings/settings.service.ts
+++ src/api/settings/settings.service.ts
@@ -22,9 +22,10 @@ export class SettingsService {
       setting: message,
       // ...info,
     };
-    const oldInfo = await this.prismaService.setting.findFirst({
-      where: { address },
-    });
+    return 1;
+    // const oldInfo = await this.prismaService.setting.findFirst({
+    //   where: { address },
+    // });
     // let result = {};
     // if (oldInfo) {
     //   result = await this.prismaService.setting.update({
