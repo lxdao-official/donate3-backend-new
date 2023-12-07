f53907221a9fdad68a68131279d4941a25e297a9
wwei-github <418047394@qq.com>
2023-12-07 14:04:40 +0800

    feat: add setting api

---
+++ prisma/migrations/20231207060128_init/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "Setting" ADD COLUMN     "avatar" TEXT,
+ADD COLUMN     "color" TEXT,
+ADD COLUMN     "description" TEXT,
+ADD COLUMN     "name" TEXT,
+ADD COLUMN     "telegram" TEXT,
+ADD COLUMN     "twitter" TEXT,
+ADD COLUMN     "type" INTEGER;
--- src/api/settings/settings.controller.ts
+++ src/api/settings/settings.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 import { SettingsService } from './settings.service';
 import { CreateSettingDto } from './dto/create-setting.dto';
-
+import { ApiBody, ApiParam } from '@nestjs/swagger';
 @Controller('settings')
 export class SettingsController {
   constructor(private readonly settingsService: SettingsService) {}
 
   @Post()
+  @ApiBody({ type: CreateSettingDto })
   create(@Body() createSettingDto: CreateSettingDto) {
     return this.settingsService.create(createSettingDto);
+  }
+  @Get(':address')
+  @ApiParam({ name: 'address', type: 'string' })
+  findSetting(@Param('address') address: string) {
+    return this.settingsService.findSetting(address);
   }
 }
