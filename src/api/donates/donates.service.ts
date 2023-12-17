5d8f8ba355c609f19efcff43e22929f5a9d5888f
wwei-github <418047394@qq.com>
2023-12-17 18:31:26 +0800

    feat: formate base64

--- src/api/donates/donates.service.ts
+++ src/api/donates/donates.service.ts
@@ -320,7 +320,7 @@ export class DonatesService {
       .toBuffer();
     const base64String = roundedCornerResizer.toString('base64');
     return {
-      img: base64String,
+      img: `data:image/svg;base64,${base64String}`,
       attributes: [
         {
           trait_type: 'address',
