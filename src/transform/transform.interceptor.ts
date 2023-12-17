a3893b705d6f7a0f01221a3dd2572d274f8dd582
wwei-github <418047394@qq.com>
2023-12-17 16:38:44 +0800

    feat: formate data

--- src/transform/transform.interceptor.ts
+++ src/transform/transform.interceptor.ts
@@ -9,8 +9,15 @@ import { Observable, map } from 'rxjs';
 @Injectable()
 export class TransformInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    return next
-      .handle()
-      .pipe(map((data) => ({ data, code: 200, message: 'success' })));
+    const request = context.switchToHttp().getRequest();
+    return next.handle().pipe(
+      map((data) => {
+        if (request.path === '/donates/SBTCard') {
+          // 获取get请求参数
+          return { data };
+        }
+        return { data, code: 200, message: 'success' };
+      }),
+    );
   }
 }
